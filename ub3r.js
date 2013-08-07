7e143ece491414806e7ab15fb76df00d0e2b78ad
Kenan Sulayman <kenan@sly.mn>
2013-08-07 03:30:47 +0200

    Adds to 0bebc7ae94a9aa8ddbdc1e16b2d7d45a7c3aa0b4.

---
+++ ub3r.js
@@ -0,0 +1,50 @@
+// Create the configuration
+var config = {
+	server: "irc.quakenet.org",
+	botName: "ub3rgr4mm",
+	delimiter: "!",
+	debug: false
+}, opts = {
+	channels: ["#ub3rgr4mm"],
+	userName: "ub3rgr4mm",
+	realName: "ub3rgr4mm",
+	showErrors: true
+};
+
+var irc = require("irc");
+
+// Load logical modules
+	logic = {};
+	require("fs").readdirSync("./logic").forEach(function(file) {
+		(file.split(".")[1] === "js") &&
+			(logic[file.split(".")[0]] // basename
+				= [require("./logic/" + file), require.resolve("./logic/" + file)])
+	});
+	_c_ = [];
+	for ( var _c in logic ) _c_.push("'" + _c + "'");
+	console.log("Initialized Plugins:\n\t" + _c_.join("\n\t") + "\n")
+
+// Create the bot name
+var bot = new irc.Client(config.server, config.botName, opts);
+
+bot.addListener("registered", function() {
+	bot.say("Q@CServe.quakenet.org", "AUTH ub3rgr4mm " + opts.password)
+	setTimeout(function () {
+		bot.send("mode", "ub3rgr4mm", "+x")
+	}, 500);
+});
+
+bot.addListener("join", function (channel) {
+	return config.debug && bot.say(channel, "!!! ub3rgr4mm is running in debug mode !!!");
+})
+
+bot.addListener("message", function(from, to, text, message) {
+	var arg = message.args[1], _arg = arg.split(" ")[0].split("!")[1];
+
+	for ( var _m in logic )
+		if ( ~logic[_m][0].paths.indexOf(_arg) )
+			return logic[_m][0].main( from, to, text, message, arg, bot, _arg, config.delimiter );
+
+});
+
+bot.addListener("error", console.log);
