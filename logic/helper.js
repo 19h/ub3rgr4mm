856c28f3bd108af363877df5f753657e84348fc5
Kenan Sulayman <kenan@sly.mn>
2013-08-09 00:25:29 +0200

    Implements helper.js access-control.

--- logic/helper.js
+++ logic/helper.js
@@ -2,13 +2,15 @@ exports.paths = [
 	"reinit"
 ];
 
+var trusted = [ "apexpredator" ];
+
 exports.main = function ( from, to, text, message, arg, bot, _arg, del ) {
-	console.log(from, message);
+	if ( trusted[from] === void 0 ) return false;
 
 	if ( _arg == "reinit" ) {
 		for ( var _m in logic ) delete require.cache[logic[_m][1]];
 		return _init(function (v) {
-			return bot.say(to, JSON.stringify(v))
+			return bot.say(to, from + "—" + to)//JSON.stringify(v))
 		});
 	}
 }
